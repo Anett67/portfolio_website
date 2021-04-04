b79f815195c69d8787723934009f8919f089e961
Anett David <davidanett@gmail.com>
2021-04-04 16:08:09 +0200

    Install Sass and create basis scss folder structure

--- assets/app.js
+++ assets/app.js
@@ -6,10 +6,9 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 import $ from 'jquery';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
 // start the Stimulus application
