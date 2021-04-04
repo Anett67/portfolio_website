6d430625bd0f62c05ba8365be8f398fd4a1131ec
Anett David <davidanett@gmail.com>
2021-04-04 20:59:21 +0200

    Send projects data to homepage in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ProjectRepository $repository): Response
     {
+        $projects = $repository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'projects' => $projects
         ]);
     }
 }
