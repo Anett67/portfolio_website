eeff73e3d423cb39051501bf15383259913be94b
Anett David <davidanett@gmail.com>
2021-04-04 14:47:33 +0200

    Add jquery, bootstrap and add entry tags to base template

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,10 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
 // start the Stimulus application
 import './bootstrap';
+
